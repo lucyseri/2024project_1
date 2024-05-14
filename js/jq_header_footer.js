5a6d43c16f321f4671e73180f8d9c05c10b5cc16
Seri Lee <serirucy@gmail.com>
2024-05-15 01:12:25 +0900

    headercommit

--- js/jq_header_footer.js
+++ js/jq_header_footer.js
@@ -1,4 +1,44 @@
 'use strict';
+const depth2Ul01 = $('ul.ul_1').outerHeight(true)+"px";
+const depth2Ul02 = $('ul.ul_2').outerHeight(true)+"px";
+const depth2Ul03 = $('ul.ul_3').outerHeight(true)+"px";
+const depth2Ul04 = $('ul.ul_4').outerHeight(true)+"px";
+const depth2Ul05 = $('ul.ul_5').outerHeight(true)+"px";
+const depth2Ul06 = $('ul.ul_6').outerHeight(true)+"px";
+const depth2Ul07 = $('ul.ul_7').outerHeight(true)+"px";
+
+const gnbtitle1 = $('.slide-gnb-1');
+const gnbtitle2 = $('.slide-gnb-2');
+const gnbtitle3 = $('.slide-gnb-3');
+const gnbtitle4 = $('.slide-gnb-4');
+const gnbtitle5 = $('.slide-gnb-5');
+const gnbtitle6 = $('.slide-gnb-6');
+const gnbtitle7 = $('.slide-gnb-7');
+
+const slideLnb1 = $('.slide-lnb-1');
+const slideLnb2 = $('.slide-lnb-2');
+const slideLnb3 = $('.slide-lnb-3');
+const slideLnb4 = $('.slide-lnb-4');
+const slideLnb5 = $('.slide-lnb-5');
+const slideLnb6 = $('.slide-lnb-6');
+const slideLnb7 = $('.slide-lnb-7');
+
+var depth2UlHeight = [
+  depth2Ul01, depth2Ul02, depth2Ul03, 
+  depth2Ul04, depth2Ul05, depth2Ul06, depth2Ul07
+];
+var gnbTitleArr = [
+  gnbtitle1, gnbtitle2, gnbtitle3, gnbtitle4,
+  gnbtitle5, gnbtitle6, gnbtitle7
+]
+var slideLnbArr = [
+  slideLnb1, slideLnb2, slideLnb3, slideLnb4,
+  slideLnb5, slideLnb6, slideLnb7
+]
+for(var a = 0; a<gnbTitleArr.length; a++){
+  gnbTitleArr[a].css("height", depth2UlHeight[a])
+  slideLnbArr[a].css("height", depth2UlHeight[a])
+}
 
 const mobileMenuOnBtn = $('.mobile-navi');
 const mobileMenuOffBtn = $('.closed-btn');
@@ -36,8 +76,6 @@ searchIcon.on('click', function(){
 searchClosed.on('click', function(){
   search.css("display", "none");
 });
-
-
 
 const footerFamilySiteDownBtn = $('.bottom-footer .sitemap button.down');
 const footerFamilySiteUpBtn = $('.bottom-footer .sitemap button.up');
